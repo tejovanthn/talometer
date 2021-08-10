fb5e3528e911b318978a0f89a80f6d94c87406e9
Tejovanth N <tj@peppercontent.in>
2021-08-10 12:22:47 +0530

    adds dropdown

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,40 @@
 <script lang="ts">
   import Talometer from "./talometer";
-  const talometer = new Talometer()
+  import Dropdown from "./components/Dropdown.svelte";
+  const talometer = new Talometer();
+
+  const tala = [
+    { value: "Dhruva", id: "IOII" },
+    { value: "Matya", id: "IOI" },
+    { value: "Rupaka", id: "OI" },
+    { value: "Jhampa", id: "IUO" },
+    { value: "Triputa", id: "IOO" },
+    { value: "Ata", id: "IIOO" },
+    { value: "Eka", id: "I" },
+  ];
+
+  const jati = [
+    { value: "Tisra", id: 4 },
+    { value: "Chatusra", id: 3 },
+    { value: "Khanda", id: 5 },
+    { value: "Misra", id: 7 },
+    { value: "Sankeerna", id: 9 },
+  ];
 
   const handleClick = (e) => {
     switch (e.target.id) {
       case "pauseplay":
-        talometer.toggle()
+        talometer.toggle();
         break;
       default:
         break;
     }
   };
+  talometer.play();
 </script>
 
 <main>
+  <Dropdown options={tala} />
+  <Dropdown options={jati} />
   <button id="pauseplay" on:click={handleClick}>Start</button>
 </main>
---
+++ src/components/Dropdown.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let options;
+    let selected;
+
+    const handleChange = (e) => {
+        selected = e.target.value;
+        console.log(selected);
+    };
+</script>
+
+<select value={selected} on:blur={handleChange}>
+    {#each options as option}
+        <option value={option.id}>
+            {option.value}
+        </option>
+    {/each}
+</select>
+
+<style>
+</style>
