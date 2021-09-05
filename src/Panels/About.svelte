097786386a82b0099d69460bb767edc27bf15960
Tejovanth N <tj@peppercontent.in>
2021-09-05 14:26:16 +0530

    fixes #6

---
+++ src/Panels/About.svelte
@@ -0,0 +1,27 @@
+<div>
+  <h2>Using Tala Pettige</h2>
+  <ul>
+    <li>Set the desired Tala, Jati, Nadai, Pitch using the dropdowns.</li>
+    <li>Set the desired BPM using the +/- 5 and */ 2 buttons.</li>
+    <li>Press Play to start the tala pettige</li>
+    <li>
+      You'll need to stop and start the pettige for any changes to take effect.
+    </li>
+    <li>You can press on the squares to mute the akshara</li>
+  </ul>
+  <hr />
+  <h2>Support</h2>
+  <p>
+    Reach out to me on <a href="https://twitter.com/ibakasura" target="_blank"
+      >Twitter</a
+    >
+    for any issues you have with the tala pettige.
+  </p>
+  <p>
+    You can also
+    <a href="https://buymeacoffee.com/tejovanthn" target="_blank"
+      >buy me a coffee</a
+    > if you like :)
+  </p>
+  <hr />
+</div>
